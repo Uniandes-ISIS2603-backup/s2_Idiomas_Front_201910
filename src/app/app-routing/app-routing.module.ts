228144cab49e54d799ccadab5a4d24efbc9c374f
j.barbosaj <j.barbosaj@uniandes.edu.co>
2019-04-11 07:24:17 -0500

    merge

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,8 +3,8 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {NgxPermissionsGuard} from 'ngx-permissions';
 
-import {AuthLoginComponent} from '../auth/auth-login/auth-login.component';
-import {AuthSignUpComponent} from '../auth/auth-sign-up/auth-sign-up.component';
+import { AuthLoginComponent } from '../auth/auth-login/auth-login.component';
+import { AuthSignUpComponent } from '../auth/auth-sign-up/auth-sign-up.component';
 import {ComentarioBlogListComponent} from '../comentario-blog/comentario-blog-list/comentario-blog-list.component';
 import {ComentarioGrupoInteresListComponent} from '../comentario-grupo-interes/comentario-grupo-interes-list/comentario-grupo-interes-list.component';
 import {ComentarioBlogDetailComponent } from '../comentario-blog/comentario-blog-detail/comentario-blog-detail.component';
@@ -236,6 +236,27 @@ const routes: Routes = [
             {
                 path: 'create',
                 component: AnfitrionCreateComponent,
+
+            }
+        ]
+    }
+    ,
+    {
+        path: 'usuarios',
+        children: [
+            {
+                path: 'list',
+                component: UsuarioListComponent
+            },
+            {
+                path: ':id',
+                component: UsuarioDetailComponent,
+                outlet: 'detail'
+            }
+            ,
+            {
+                path: 'create',
+                component: UsuarioCreateComponent,
 
             }
         ]
